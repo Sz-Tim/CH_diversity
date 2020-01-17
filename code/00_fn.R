b1f5e429cd7941dd07fa1b16a6cdc5c6269ac7fa
Sz-Tim <tim.szewczyk@gmail.com>
2020-01-17 07:21:38 +0100

    generalized functions

--- code/00_fn.R
+++ code/00_fn.R
@@ -16,24 +16,34 @@ make_X <- function(K, R, quad) {
 
 
 
-make_slopes <- function(lam_0, R, G, S, tax_i, sd_sp, quad) {
-  # aggregate: alpha[R], beta[R]
+make_V <- function(I, L) {
+  nTot <- sum(unlist(I)) 
+  V <- matrix(rnorm(nTot*L, 0, 1), nrow=nTot, ncol=L)
+  return(list(all=V,
+              Y=V[1:I$Y,],
+              Y_=V[(1:I$Y_)+I$Y,]))
+}
+
+
+
+make_slopes <- function(lam_0, nCov, G, S, tax_i, sd_sp, quad) {
+  # aggregate: alpha[nCov], beta[nCov]
   if(!is.null(lam_0)) {
-    agg <- cbind(c(lam_0, rnorm(R-1, 0, 1)))
+    agg <- cbind(c(lam_0, rnorm(nCov-1, 0, 1)))
   } else {
-    agg <- rnorm(R, 0, 1)
+    agg <- cbind(rnorm(nCov, 0, 1))
   }
-  if(quad) agg[R] <- ifelse(agg[R] < 0, 2*agg[R], -2*agg[R])
+  if(quad) agg[nCov] <- ifelse(agg[nCov] < 0, 2*agg[nCov], -2*agg[nCov])
   
-  # genus-level: A[G,R], B[G,R]
-  phy_covMX <- matrix(runif(G^2)*2-1, ncol=G) 
+  # genus-level: A[G,nCov], B[G,nCov]
+  phy_covMX <- matrix(runif(G^2)*2-1, ncol=G)/2 
   diag(phy_covMX) <- diag(phy_covMX)
   Sigma <- t(phy_covMX) %*% phy_covMX
   gen <- t(apply(agg, 1, function(x) mvrnorm(1, rep(x,G), Sigma)))
   
-  # species-level: a[S,R], b[S,R]
-  sp <- matrix(ncol=S, nrow=R)
-  for(r in 1:(R)) {
+  # species-level: a[S,nCov], b[S,nCov]
+  sp <- matrix(ncol=S, nrow=nCov)
+  for(r in 1:nCov) {
     sp[r,] <- rnorm(S, gen[r,tax_i[,2]], sd_sp)
   }
   return(list(agg=agg, gen=gen, sp=sp))
@@ -46,7 +56,9 @@ aggregate_aggSlopes <- function(out.ls, slope.ls, var) {
   true <- data.frame(value=c(slope.ls$agg),
                      Parameter=paste0(var, "[", 1:length(slope.ls$agg), "]"),
                      R=as.character(1:length(slope.ls$agg)))
-  return(list(gg=gg, true=true))
+  post_mns <- gg %>% group_by(model, Parameter) %>%
+    summarise(mean=mean(value), median=median(value))
+  return(list(gg=gg, true=true, post_mns=post_mns))
 }
 
 
@@ -92,18 +104,54 @@ aggregate_D <- function(out.ls, D) {
 
 
 aggregate_Lambda <- function(out.ls, LAMBDA) {
-  gg <- map_dfr(out.ls, ~ggs(., "LAMBDA_"), .id="model")
-  true <- map2_dfr(LAMBDA[-1], names(LAMBDA)[-1],
-                   ~tibble(true=c(.x), 
-                           K=as.character(rep(1:dim(.x)[1], times=dim(.x)[2])),
-                           S=as.character(rep(1:dim(.x)[2], each=dim(.x)[1]))),
-                   .id="dataset") %>%
-    mutate(Parameter=paste0("LAMBDA_", dataset, "[", K, ",", S, "]"))
+  Lam_sim <- list(LAMBDA=rbind(LAMBDA$W, LAMBDA$Y),
+                  LAMBDA_=rbind(LAMBDA$W_, LAMBDA$Y_))
+  gg <- map_dfr(out.ls, ~ggs(., "LAMBDA"), .id="model")
+  true <- map_dfr(Lam_sim, 
+                  ~tibble(true=c(.), 
+                          K=as.character(rep(1:dim(.)[1], times=dim(.)[2])),
+                          S=as.character(rep(1:dim(.)[2], each=dim(.)[1]))),
+                  .id="dataset") %>%
+    mutate(Parameter=paste0(dataset, "[", K, ",", S, "]"))
+  # true <- map_dfr(LAMBDA[-1],
+  #                  ~tibble(true=c(.), 
+  #                          K=as.character(rep(1:dim(.)[1], times=dim(.)[2])),
+  #                          S=as.character(rep(1:dim(.)[2], each=dim(.)[1]))),
+  #                  .id="dataset") %>%
+  #   mutate(Parameter=paste0("LAMBDA", "[", K, ",", S, "]"))
   sum.gg <- gg %>% group_by(model, Parameter) %>%
     summarise(mn=mean(value), 
               med=median(value),
               q025=quantile(value, probs=0.025),
-              q975=quantile(value, probs=0.975)) %>%
+              q975=quantile(value, probs=0.975),
+              lmn=mean(log(value)), 
+              lmed=median(log(value)),
+              lq025=quantile(log(value), probs=0.025),
+              lq975=quantile(log(value), probs=0.975)) %>%
+    full_join(., true, by="Parameter") %>%
+    mutate(train=c("train", "test")[grepl("_", dataset)+1])
+  return(list(gg=gg, sum.gg=sum.gg, true=true))
+}
+
+
+
+aggregate_lambda <- function(out.ls, lambda) {
+  gg <- map_dfr(out.ls, ~ggs(., "lambda"), .id="model")
+  true <- map_dfr(setNames(lambda, c("lambda", "lambda_")),
+                   ~tibble(true=c(.), 
+                           K=as.character(rep(1:dim(.)[1], times=dim(.)[2])),
+                           S=as.character(rep(1:dim(.)[2], each=dim(.)[1]))),
+                   .id="dataset") %>%
+    mutate(Parameter=paste0(dataset, "[", K, ",", S, "]"))
+  sum.gg <- gg %>% group_by(model, Parameter) %>%
+    summarise(mn=mean(value), 
+              med=median(value),
+              q025=quantile(value, probs=0.025),
+              q975=quantile(value, probs=0.975),
+              lmn=mean(log(value)), 
+              lmed=median(log(value)),
+              lq025=quantile(log(value), probs=0.025),
+              lq975=quantile(log(value), probs=0.975)) %>%
     full_join(., true, by="Parameter") %>%
     mutate(train=c("train", "test")[grepl("_", dataset)+1])
   return(list(gg=gg, sum.gg=sum.gg, true=true))
