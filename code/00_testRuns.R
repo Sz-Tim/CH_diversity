fc2070d17b9450d82b2efe76da0e3983a6205383
Sz-Tim <tim.szewczyk@gmail.com>
2020-04-14 11:55:09 +0200

    streamlined code

--- code/00_testRuns.R
+++ code/00_testRuns.R
@@ -4,61 +4,72 @@
 
 
 ##--- set up
-library(rstan); library(tidyverse); library(sf); library(googlesheets)
-options(mc.cores=parallel::detectCores())
-rstan_options(auto_write=TRUE)
-gis.dir <- "../2_gis/data/VD_21781/"
-ant.dir <- "../1_opfo/data/"
-tax_i <- read_csv("data/tax_i.csv") #%>% filter(sNum<50)
-plot_i <- read_csv(paste0(ant.dir, "opfo_envDataProcessed.csv")) %>% 
-  arrange(BDM, Plot_id) %>% filter(Plot_id != "020201")
-source("code/00_fn.R"); source(paste0(ant.dir, "../code/00_fn.R"))
+library(rstan); library(tidyverse); library(foreach); library(doSNOW)
+source("code/00_fn.R")
 
 
 
 ##--- settings
-X_vars <- c("MAT", "Iso", "AP", "npp", "MAT_sq", "lcH")
-V_vars <- c("el", "slope", "pop")
-U_vars <- c("slope", "pop", "rdLen")
+X_vars <- c("MAT", "lcH", "npp", "MAT_sq")
+V_vars <- c("SoilTSt", "CnpyOpn", "CnpyMxd", "VegTot")
+U_vars <- c("pop", "rdLen")
 
 
 
 # Testing
-mods <- "W" #c("W", "Y", "WY")
+mods <- c("W", "Y", "WY")
 
-pars.keep <- c("sigma_b", "beta", "L_Omega_B", 
-               "sigma_a", "alpha", "L_Omega_A",
+pars.keep <- c("sigma_b", "beta", "Sigma_B", 
+               "sigma_a", "alpha", "Sigma_A",
+               "b", "B", "a", "A",
+               "log_lik_lambda_",
                "eta", "D",
+               "lLAMBDA", "lLAMBDA_",
                "ShannonH", "ShannonH_")
 pars.excl <- c("b_std", "B_std", 
                "a_std", "A_std",
-               "b", "B", "a", "A",
-               "log_lik_lambda_",
-               "LAMBDA", "lambda", 
+               "L_Omega_B", "L_Omega_A",
+               "sigma_B", "sigma_A",
+               "L_Omega_a", "L_Omega_a",
+               "LAMBDA", "LAMBDA_", 
+               "lambda", "lambda_", 
+               "llambda", "llambda_", 
                "E", "E_",
-               "LAMBDA_", "lambda_", 
                "p", "p_")
 
 
-out.ls <- setNames(vector("list", length(mods)), mods)
-for(i in seq_along(mods)) {
-  out.ls[i] <- stan(file=paste0("code/mods/PPM_mvPhy_", mods[i], "_IJK.stan"),
-                    # sample_file=paste0("out/tests/", i), 
-                    pars=pars.excl, include=F, chains=3,
-                    data=read_rdump("data/stan_data/test_realData.Rdump"), 
-                    thin=5, warmup=500, iter=1000)
+p.c <- makeCluster(3); registerDoSNOW(p.c)
+out.ls <- foreach(i=seq_along(mods), .packages="rstan") %dopar% {
+  options(mc.cores=1)
+  rstan_options(auto_write=TRUE)
+  out <- stan(file=paste0("code/mods/PPM_mvPhy_", mods[i], "_IJK.stan"),
+       sample_file=paste0("out/tests/", mods[i]),
+       pars=pars.excl, include=F, chains=1,
+       data=read_rdump("data/stan_data/test_realData.Rdump"), 
+       warmup=50, iter=100)
+  saveRDS(out, paste0("out/tests/test_", mods[i], ".rds"))
 }
+stopCluster(p.c)
 
-d.ls <- read_rdump("data/stan_data/test_realData.Rdump")
-matrix(rstan::summary(out.ls$W, pars="L_Omega_A")$summary[,1], 
-       nrow=d.ls$G, byrow=T)
-matrix(rstan::summary(out.ls$W, pars="L_Omega_B")$summary[,1], 
-       nrow=d.ls$G, byrow=T)
-cbind(rstan::summary(out.ls$WY, pars=c("sigma_a", "sigma_b"))$summary[,1])
-cbind(rstan::summary(out.ls$WY, pars=c("alpha"))$summary[,1])
-cbind(rstan::summary(out.ls$WY, pars=c("beta"))$summary[,1])
-cbind(rstan::summary(out.ls$WY, pars=c("eta"))$summary[,1])
-cbind(rstan::summary(out.ls$WY, pars=c("D"))$summary[,1])
+# out.ls <- setNames(vector("list", length(mods)), mods)
+# for(i in seq_along(mods)) {
+#   out.ls[i] <- stan(file=paste0("code/mods/PPM_mvPhy_", mods[i], "_IJK.stan"),
+#                     sample_file=paste0("out/tests/", mods[i]),
+#                     pars=pars.excl, include=F, chains=2,
+#                     data=read_rdump("data/stan_data/test_realData.Rdump"), 
+#                     thin=5, warmup=100, iter=200)
+# }
+
+# d.ls <- read_rdump("data/stan_data/test_realData.Rdump")
+# matrix(rstan::summary(out.ls$W, pars="L_Omega_A")$summary[,1], 
+#        nrow=d.ls$G, byrow=T)
+# matrix(rstan::summary(out.ls$W, pars="L_Omega_B")$summary[,1], 
+#        nrow=d.ls$G, byrow=T)
+# cbind(rstan::summary(out.ls$WY, pars=c("sigma_a", "sigma_b"))$summary[,1])
+# cbind(rstan::summary(out.ls$WY, pars=c("alpha"))$summary[,1])
+# cbind(rstan::summary(out.ls$WY, pars=c("beta"))$summary[,1])
+# cbind(rstan::summary(out.ls$WY, pars=c("eta"))$summary[,1])
+# cbind(rstan::summary(out.ls$WY, pars=c("D"))$summary[,1])
 
 
 
