394966a5c99e73757595d768e23329dbfb17502f
Sz-Tim <tim.szewczyk@gmail.com>
2021-01-08 13:44:41 +0100

    variable selection code

---
+++ code/02-10/02-10-3_Y.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 02-10-3_Y.sh
+# Tim Szewczyk
+#
+# This script runs models with one covariate through cmdstan: 
+#    code/mods/Y_fwdSearch
+# Dataset is data/fwdSearch/Y_10__*.Rdump
+# Output is stored in out/fwdSearch/
+
+# warmup and sampling per chain
+nWarm=2000
+nSamp=500
+
+FILES=data/fwdSearch/Y_10__*.Rdump
+d=($FILES)
+dfull=($(basename -a $FILES))
+
+for f in {16..19}
+do 
+  df="${dfull[$f]}"
+  dname="${df%.Rdump}"
+  out="out/fwdSearch/"
+  out+=$dname
+  out+="_"
+  
+  for j in {1..3}
+  do
+	  outj=$out
+	  outj+=$j
+	  code/mods/Y_fwdSearch sample \
+   	    algorithm=hmc \
+   	      engine=nuts \
+          metric=diag_e \
+        num_samples=$nSamp \
+        num_warmup=$nWarm \
+        data file="${d[$f]}" \
+        init=0 \
+        output file=$outj.csv \
+        refresh=10 &
+  done
+done
+
+wait
