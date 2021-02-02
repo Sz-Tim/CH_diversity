ae289ca618b832366e47fb690ed3849c66ef9b46
Sz-Tim <tim.szewczyk@gmail.com>
2021-02-02 15:14:58 +0100

    null models for mcfadden's pseudo-R2

--- code/03_make_opt_data.R
+++ code/03_make_opt_data.R
@@ -20,23 +20,28 @@ loo.f <- dir(fS_out, "loo.*csv")
 loo.df <- tibble(mod=str_split_fixed(loo.f, "_", 3)[,2], 
                  nCov=as.numeric(str_split_fixed(loo.f, "_", 4)[,3]), 
                  elpd=map_dbl(loo.f, ~read_csv(paste0(fS_out, .x))$elpd_loo[1]),
-                 elpd_se=map_dbl(loo.f, ~read_csv(paste0(fS_out, .x))$se_elpd_loo[1]),
+                 looic=map_dbl(loo.f, ~read_csv(paste0(fS_out, .x))$looic[1]),
                  v_full=map_chr(loo.f, ~read_csv(paste0(fS_out, .x))$X1[1])) %>%
-  group_by(mod) %>% filter(abs(elpd-max(elpd))<4) %>% 
-  # filter(nCov == max(nCov)) %>% ungroup %>%
-  filter(elpd == max(elpd)) %>% ungroup %>%
+  group_by(mod) %>% filter(abs(looic-min(looic))<4) %>% 
+  filter(looic == min(looic)) %>% ungroup %>%
   mutate(file=paste(mod, nCov, "_k-1", str_split_fixed(v_full, "__", 2)[,2], sep="_"))
 opt_var <- map(loo.df$file, ~readRDS(paste0(fS_dat, .x, "_ls.rds"))) %>%
   setNames(loo.df$mod) %>%
   map(~c(colnames(.$X), colnames(.$V)))
-
+null_var <- list("WY"="R_", "Y"="R_")
 
 
 
 
 ##### Make datasets     --------------------------------------------------------
+# optimal variable sets
 make_next_datasets(full_data_base=d.f, out_base=paste0(opt_dat, "Y"), 
                    opt_names=opt_var$Y, type="pred", mod_size=length(opt_var$Y))
 make_next_datasets(full_data_base=d.f, out_base=paste0(opt_dat, "WY"), 
                    opt_names=opt_var$WY, type="pred", mod_size=length(opt_var$WY))
 
+# intercept only null models
+make_next_datasets(full_data_base=d.f, out_base=paste0(opt_dat, "Y_null"), 
+                   opt_names=null_var$Y, type="pred", mod_size=length(null_var$Y))
+make_next_datasets(full_data_base=d.f, out_base=paste0(opt_dat, "WY_null"), 
+                   opt_names=null_var$WY, type="pred", mod_size=length(null_var$WY))
