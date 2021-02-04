80035c962254b128d6fbb17aa382a3b130b46a03
Sz-Tim <tim.szewczyk@gmail.com>
2021-02-04 09:06:52 +0100

    processing null model

--- code/05_aggregateOuput.R
+++ code/05_aggregateOuput.R
@@ -27,6 +27,27 @@ saveRDS(agg_vs$full, "out/agg_vs_k-2_full.rds")
 
 
 
+### Null models ----------------------------------------------------------------
+
+agg_null <- aggregate_output(d.f="data/opt/Y_null__opt_var_set",
+                             mods="Y_null__",
+                             out.dir="/VOLUMES/Rocinante/opfo_div_cluster/null/",
+                             pars_save=c("lLAMBDA", "lLAMBDA_", "llambda",
+                                         "tot_lLAM", "tot_lLAM_", "tot_llam", 
+                                         "prPres", "prPres_", "prPresL", 
+                                         "ShannonH", "ShannonH_", "ShannonH_L",
+                                         "Rich", "Rich_", "RichL",
+                                         "pred_Y", "pred_Y_", "pred_YL",
+                                         "beta", "B", "b", "sigma_b", "Sigma_B",
+                                         "disp_lam", "D", "log_lik"))
+
+saveRDS(agg_null$summaries, "out/agg_null_Y.rds")
+saveRDS(agg_null$full, "out/agg_null_full_Y.rds")
+
+
+
+
+
 ### Best models ----------------------------------------------------------------
 
 agg_opt <- aggregate_output(d.f="data/opt/Y__opt_var_set",
